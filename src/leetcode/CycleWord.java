8efbbd1e89f24b2a6cb99310e38b355544c6a18e
WangJian <826204781@qq.com>
2017-04-06 14:18:12 +0800

    正确版

--- src/leetcode/CycleWord.java
+++ src/leetcode/CycleWord.java
@@ -1,8 +1,6 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by 1 on 2017/4/2.
@@ -11,23 +9,54 @@ public class CycleWord {
     /*
     * 如果一个单词通过循环右移获得的单词，我们称这些单词都为一种循环单词。
     * 例如：picture 和 turepic 就是属于同一种循环单词。
-     * 现在给出n个单词，需要统计这个n个单词中有多少种循环单词
+     * 现在给出n个单词，需要统计这个n个单词中有多少种单词
     * */
-
-    public static String move(String string) {
-        char[] chars = string.toCharArray();
-        char[] charTemp = new char[string.length()];
-
-        for (int i = 0,j = 0; i < chars.length - 1; j++, i++) {
-            charTemp[j] = chars[i+1];
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = 0;
+        int num = in.nextInt();
+        String[] strs = new String[num];
+        for (int i = 0; i < num; i++) {
+            strs[i] = in.next();
         }
-        charTemp[chars.length - 1] = chars[0];
-        return String.valueOf(charTemp);
+        System.out.println(function(strs));
     }
 
-    public static void main(String[] args) {
-        String str = "abcd";
-        System.out.println(str = move(str));
-        System.out.println(move(str));
+    private static int function(String[] strings) {
+        int temp = 0;
+        int sum = 0;
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < strings.length; i++) {
+            Iterator<String> it = arrayList.iterator();
+            while (it.hasNext()) {
+                if (isExist(strings[i],it.next())) {
+                    temp = 1;
+                }
+            }
+            if (temp != 1) {
+                sum++;
+                arrayList.add(strings[i]);
+            } else {
+                temp = 0;
+            }
+        }
+        return sum;
+    }
+
+    private static boolean isExist(String left, String right) {
+        int lenl = left.length();
+        int lenr = right.length();
+
+        if (lenl != lenr || lenl == 0 || lenr == 0) {
+            return false;
+        }
+
+        left = left.concat(left);
+
+        if (left.contains(right)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
